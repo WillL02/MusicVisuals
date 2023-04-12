5a7f73008b2bba8bb51ced7223d4a238da71c5c8
Ciaran Cruz <C21318233@mytudublin.ie>
2023-04-12 14:20:32 +0100

    Finished line visual. Got the timing for third drop of the song

--- java/src/C21318233/Heathens.java
+++ java/src/C21318233/Heathens.java
@@ -13,46 +13,55 @@ public class Heathens extends Visual {
 
     public void setup() {
 
-        //Start playing audio
+        // Start playing audio
         startMinim();
         loadAudio("heathens.mp3");
         getAudioPlayer().play();
 
-        //Initialising rain drops at random x and y values
+        // Initialising rain drops at random x and y values
         for (int i = 0; i < d.length; i++) {
             d[i] = new Rain(this, random(0, width), random(-1000, 0));
         }
+
     }
 
     public void draw() {
 
-        //Used for syncing the song to raindrops
+        // Used for syncing the song to raindrops
         rainSync();
 
-        //First drop of the song (Changes background)
-        if (millis() > 25895)
-        {
-            background(47,79,79);
-        }
-        else
-        {
+        // First drop of the song (Changes background)
+        if (millis() > 25895 && millis() < 46000) {
+            background(47, 79, 79);
+
+        } else if (millis() < 25895) {
             background(0);
         }
-        
-        //Rain effect
-        for (int i = 0; i < d.length; i++) {
-            d[i].fall(lerpedAverage);
-            d[i].show();
-            d[i].bottom();
-        }
-        
-        bouncingCircle();
 
-        //Second drop of the song (Start of line visual)
-        if (millis() > 47000)
+        //For transition between first and second drop of song
+        if (millis() < 47000) {
+            // Rain effect
+            for (int i = 0; i < d.length; i++) {
+                d[i].fall(lerpedAverage);
+                d[i].show();
+                d[i].bottom();
+            }
+            bouncingCircle();
+        }
+        else if (millis() > 47000 && millis() < 47100)
         {
-            stroke(255,0,0);
-            line(0,0, 500, 500); //For testing purposes
+            background(47, 79, 79);
+        }
+
+        // Second drop of the song (Start of line visual)
+        if (millis() > 47000) {
+            lineVisual();
+        }
+
+        //Third drop of the song
+        if (millis() > 58000)
+        {
+            background(47, 79, 79);
         }
 
     }
@@ -60,35 +69,31 @@ public class Heathens extends Visual {
     public void bouncingCircle() {
         float halfheight = height / 2;
         float halfwidth = width / 2;
-        
-        //For syncing with song
+
+        // For syncing with song
         calculateAverageAmplitude();
 
-        //Draws circle
+        // Draws circle
         strokeWeight(3);
 
-        //Changes color of circle on the first drop of the song
-        if (millis() > 25895)
-        {
+        // Changes color of circle on the first drop of the song
+        if (millis() > 25895) {
             stroke(255);
-            fill(47,79,79);
-        }
-        else
-        {
-            stroke(255,0,0);
+            fill(47, 79, 79);
+        } else {
+            stroke(255, 0, 0);
             fill(0);
-            
+
         }
 
         circle(halfwidth, halfheight, getSmoothedAmplitude() * 2000);
     }
 
-    
+    // For syncing song
     public void rainSync() {
-        //For syncing song
+
         float total = 0;
-        for (int i = 0; i < getAudioBuffer().size(); i++) 
-        {
+        for (int i = 0; i < getAudioBuffer().size(); i++) {
             total += abs(getAudioBuffer().get(i));
         }
         float average = total / (float) getAudioBuffer().size();
@@ -96,6 +101,8 @@ public class Heathens extends Visual {
     }
 
     public void lineVisual() {
-        
+        stroke(random(50, 255));
+        strokeWeight(7);
+        line(random(width), random(height), random(width), random(height));
     }
 }
